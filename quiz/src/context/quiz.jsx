316d61a36ba0066d1dcacb23685312465db1677c
Samuel Souza <smksouza.dev@gmail.com>
2023-06-15 00:28:04 -0300

    criando a alteração dos estagios

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -1,4 +1,22 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions'
+
+
+const STAGES = ["start", "Playing", "End"]
+
+const initialState = {
+  gameStage: STAGES[0],
+  questions
+}
+
+const quizReducer = (state, action) => {
+  switch(action.type) {
+    case "CHANGE_STATE":
+      return state
+    default:
+      return state  
+  }
+}
 
 export const QuizContext = createContext();
 
